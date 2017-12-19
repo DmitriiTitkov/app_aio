c431a0bbf3117c8b0296ffb4b3a206b0cebfff92
Dmitrii Titkov <Dmitrii_Titkov@epam.com>
2017-12-19 20:10:59 +0300

    Validation added

--- handlers/html.py
+++ handlers/html.py
@@ -2,7 +2,7 @@ from aiohttp import web
 from service.snmp import get_snmp_value
 import templates
 import aiohttp_jinja2
-
+import string
 
 
 def template(*args):
@@ -16,10 +16,24 @@ def template(*args):
 async def home(request: web.Request):
     return {'test': ""}
 
+
 @aiohttp_jinja2.template('home.html')
 async def home_post(request: web.Request):
-    # Some Validation here
+
     data = await request.post()
-    print(data)
-    testget = await get_snmp_value("sysDescr")
-    return {'test': testget}
+    oid = data.get("snmp_oid")
+
+    # validation
+    valid_oid_symbols = set(string.digits + ".")
+    if set(oid) <= valid_oid_symbols and oid:
+        if len(oid) == 1:
+            oid = tuple(oid)
+
+        snmp_result = await get_snmp_value(oid)
+        if snmp_result:
+            return {'test': snmp_result}
+        else:
+            return {'test': "Nothing was found. Check if the OID correct."}
+    else:
+        return {'test': "OID is not valid. OID should contain only digits and '.'."}
+
--- service/snmp.py
+++ service/snmp.py
@@ -2,19 +2,26 @@ import asyncio
 from pysnmp.hlapi.asyncio import *
 
 
-async def get_snmp_value(oid: int) -> str:
-    error_indication, error_status, error_index, var_binds = \
-        await getCmd(SnmpEngine(), CommunityData('public'),
-                     UdpTransportTarget(('192.168.63.10', 161)),
-                     ContextData(),
-                     ObjectType(ObjectIdentity('SNMPv2-MIB', oid, 0))
-                     )
-    if not error_indication:
-        snmp_value = ''
-        for var_bind in var_binds:
-            snmp_value += var_bind.prettyPrint() + '\n'
+async def get_snmp_value(oid: tuple) -> str:
+    try:
+        error_indication, error_status, error_index, var_binds = \
+            await getCmd(SnmpEngine(), CommunityData('public'),
+                         UdpTransportTarget(('192.168.63.10', 161)),
+                         ContextData(),
+                         ObjectType(ObjectIdentity(oid))
+                         )
+        print(error_indication, error_status, error_index, var_binds)
 
-    return snmp_value
+        if not error_indication:
+            snmp_value = ''
+            for var_bind in var_binds:
+                snmp_value += var_bind.prettyPrint() + '\n'
+            if "No Such Object currently exists at this OID" in snmp_value:
+                return False
+            else:
+                return snmp_value
+    except:
+        return False
 
 
-# res = asyncio.get_event_loop().run_until_complete(get_snmp_value(1))
+# res = asyncio.get_event_loop().run_until_complete(get_snmp_value((1, 3)))
